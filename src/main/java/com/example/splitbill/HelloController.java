337a3e697f7f3a6ff3f46415401a7276ae4ba906
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-10-17 00:11:06 +0100

    Initial commit

---
+++ src/main/java/com/example/splitbill/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.splitbill;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    public class HelloController {
+
+        @GetMapping("/")
+        public String index() {
+            return "Greetings from Spring Boot!";
+        }
+
+    }
+
---
+++ src/main/java/com/example/splitbill/SplitBillApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitBillApplication.class, args);
+	}
+
+}
