df1176de4b755d68019a5fe1e6b7c9d83e47bc75
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2021-12-30 23:48:00 +0000

    Add initial test

--- src/test/java/com/example/splitbill/ReceiptTest.java
+++ src/test/java/com/example/splitbill/ReceiptTest.java
@@ -2,13 +2,24 @@ package com.example.splitbill;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
+import com.google.cloud.vision.v1.AnnotateImageResponse;
 import org.junit.jupiter.api.Test;
 
 class ReceiptTest {
+    @Test
+    void testHenFixtureWithColumns() throws IOException {
+        FileInputStream fis = new FileInputStream("src/fixtures/HEN");
+        var air = AnnotateImageResponse.parseFrom(fis);
+        var r = Receipt.fromImageRecognitionResponse(air);
+        assertEquals(5, r.getLines().size());
+    }
     @Test
     void fromList() {
         String[] arr = new String[]{"""
