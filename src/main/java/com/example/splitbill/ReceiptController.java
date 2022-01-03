5fb4974905ee2583d9f7fa6f39178e013102eb29
Iryna-Slynko <53280378+Iryna-Slynko@users.noreply.github.com>
2022-01-03 09:48:58 +0000

    Add polling for requests

--- src/main/java/com/example/splitbill/ReceiptController.java
+++ src/main/java/com/example/splitbill/ReceiptController.java
@@ -2,6 +2,7 @@ package com.example.splitbill;
 
 import com.example.splitbill.models.Receipt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,26 +11,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReceiptController {
-        @Autowired
-        ReceiptRepository receiptRepository;
+    @Autowired
+    ReceiptRepository receiptRepository;
 
-        @GetMapping("/receipt")
-        public void receipt(@RequestParam(name = "name", required = false, defaultValue = "Owner") String name,
-        @RequestParam(name = "userid", required = false, defaultValue = "123") String ownerId,
+    @GetMapping("/receipt")
+    public void receipt(@RequestParam(name = "name", required = false, defaultValue = "Owner") String name,
+                        @RequestParam(name = "userid", required = false, defaultValue = "123") String ownerId,
                         Model model) {
-                                var r = Receipt.newDemoReceipt();
-                                r.setOwnerId(ownerId);;
-                                r.setOwnerName(name);
-                model.addAttribute("name", name);
-                model.addAttribute("receipt", r
-                                );
-        }
+        var r = Receipt.newDemoReceipt();
+        r.setOwnerId(ownerId);
+        r.setOwnerName(name);
+        model.addAttribute("name", name);
+        model.addAttribute("receipt", r
+        );
+    }
 
-        @GetMapping("/receipt/{id}")
-        public String receiptInfo(@PathVariable(value = "id") String id, Model model) {
-                Receipt r = receiptRepository.findById(id).block();
-                model.addAttribute("receipt", r);
-                return "receipt";
-        }
+    @GetMapping("/receipt/{id}")
+    public String receiptInfo(@PathVariable(value = "id") String id, Model model) {
+        Receipt r = receiptRepository.findById(id).block();
+        model.addAttribute("receipt", r);
+        return "receipt";
+    }
 
+    @GetMapping("/receipt/{id}/data")
+    public ResponseEntity<Receipt> receiptDara(@PathVariable(value = "id") String id,
+                                               @RequestParam(name = "userId", required = false, defaultValue = "123") String ownerId,
+                                               @RequestParam(name = "username", required = false, defaultValue = "123") String ownerName) {
+        return ResponseEntity.ok(receiptRepository.findById(id).block());
+    }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,42 +2,53 @@
 'use strict';
 const modal = document.getElementById('name-modal');
 const myModal = new bootstrap.Modal(modal);
-
-function enableEditing() {
-  if ((username !== "") && (username !== null)) {
-    document.querySelectorAll('input').forEach(function (element) {
-      element.removeAttribute('disabled');
-    });
-    const userIdInput = document.getElementById('userid');
-    if (userIdInput !== null) {
-      userIdInput.value = userId;
-      document.getElementById('username').value = username;
-    }
-    document.getElementById('user-header').innerText = "Welcome, " + username;
-  } else {
-    myModal.show();
-  }
-}
+const receiptTable = document.querySelector("#receipt-table");
 
 let username = localStorage.getItem('username');
 let userId = localStorage.getItem('userid') || crypto.randomUUID();
+let timeoutId = null;
 
-modal.addEventListener('hidden.bs.modal', function (event) {
-  username = document.getElementById('introduction-name').value.trim();
-  localStorage.setItem("username", username);
-  localStorage.setItem("userid", userId);
-  enableEditing();
-});
-document.getElementById('form-intro').addEventListener('submit', function (event) {
-  event.preventDefault();
-  myModal.hide();
-});
+function updateReceiptData(receipt) {
+  if (receiptTable === null) {
+    return;
+  }
 
-enableEditing();
+  if (timeoutId !== null) {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+  }
 
-const receiptTable = document.querySelector("#receipt-table");
-if (receiptTable !== null) {
   const receiptLines = receiptTable.getElementsByTagName('tr');
+  receipt.lines.forEach((line, i) => {
+    const tableLine = receiptLines[i];
+    if (line.claimedByID === null) {
+      tableLine.classList.remove('table-warning', 'table-danger');
+      const checkbox = tableLine.getElementsByTagName('input')[0];
+      checkbox.removeAttribute('disabled');
+      checkbox.checked = false;
+    } else {
+      const checkbox = tableLine.getElementsByTagName('input')[0];
+      checkbox.checked = true;
+      if (line.claimedByID === userId) {
+        checkbox.removeAttribute('disabled');
+        tableLine.classList.add('table-warning');
+      } else {
+        checkbox.disabled = "disabled";
+        tableLine.classList.add('table-danger');
+      }
+    }
+  });
+  timeoutId = setTimeout(pollGetData, 15000);
+}
+
+const pollGetData = async () => {
+  const receiptId = receiptTable.dataset.receiptId;
+  fetch('/receipt/' + receiptId + '/data?' + new URLSearchParams({userId: userId, username: username}))
+  .then(response => response.json())
+  .then(updateReceiptData);
+}
+
+if (receiptTable !== null) {
   function makePutRequest(url) {
     fetch(url, {
       method: 'PUT',
@@ -46,27 +57,7 @@ if (receiptTable !== null) {
       },
       body: JSON.stringify({ 'userId': userId, 'username': username })
     }).then(response => response.json())
-      .then(receipt => {
-        receipt.lines.forEach((line, i) => {
-          const tableLine = receiptLines[i];
-          if (line.claimedByID === null) {
-            tableLine.classList.remove('table-warning', 'table-danger');
-            const checkbox = tableLine.getElementsByTagName('input')[0];
-            checkbox.removeAttribute('disabled');
-            checkbox.checked = false;
-          } else {
-            const checkbox = tableLine.getElementsByTagName('input')[0];
-            checkbox.checked = true;
-            if (line.claimedByID === userId) {
-              checkbox.removeAttribute('disabled');
-              tableLine.classList.add('table-warning');
-            } else {
-              checkbox.disabled = "disabled";
-              tableLine.classList.add('table-danger');
-            }
-          }
-        });
-      })
+      .then(updateReceiptData);
   }
 
   const receiptId = receiptTable.dataset.receiptId;
@@ -79,6 +70,8 @@ if (receiptTable !== null) {
     if (checkbox.disabled) {
       return;
     }
+    clearTimeout(timeoutId);
+    timeoutId = null;
     const rowId = checkbox.dataset.rowId;
     checkbox.checked = !checkbox.checked;
     if (checkbox.checked) {
@@ -99,3 +92,33 @@ if (receiptTable !== null) {
     }
   });
 }
+
+function enableEditing() {
+  if ((username !== "") && (username !== null)) {
+    const userIdInput = document.getElementById('userid');
+    if (userIdInput !== null) {
+      userIdInput.value = userId;
+      document.getElementById('username').value = username;
+    }
+    document.getElementById('user-header').innerText = "Welcome, " + username;
+
+    if (receiptTable !== null) {
+      pollGetData();
+    }  
+  } else {
+    myModal.show();
+  }
+}
+
+modal.addEventListener('hidden.bs.modal', function (event) {
+  username = document.getElementById('introduction-name').value.trim();
+  localStorage.setItem("username", username);
+  localStorage.setItem("userid", userId);
+  enableEditing();
+});
+document.getElementById('form-intro').addEventListener('submit', function (event) {
+  event.preventDefault();
+  myModal.hide();
+});
+
+enableEditing();
